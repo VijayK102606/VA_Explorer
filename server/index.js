48166428ec44f38e449e641028c03661c0ff5c92
Vijay Kokalla <vkokalla@ucsc.edu>
2025-09-15 06:46:31 -0700

    made changes

--- server/index.js
+++ server/index.js
@@ -9,7 +9,8 @@ const EPORT = process.env.PORT || 4000;
 const REACTURL = process.env.REACT_URL;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
 
 app.use('/api', uploadRoutes);
 
@@ -46,6 +47,44 @@ app.post('/api/chatbot', async (req, res) => {
         res.json({ answer: cleanText });
         } catch (error) {
         console.error('Chatbot error:', error?.response?.data || error.message);
+        res.status(500).json({ error: 'Failed to get response from LLM.' });
+    }
+});
+
+app.post('/api/chatbot-batch', async(req, res) => {
+    const { question, entries } = req.body;
+    if (!question || !entries || entries.length === 0) {
+        return res.status(400).json({ error: "Missing question or entires."});
+    }
+
+    try {
+        const prompt = `Given the following Verbal Autopsy data: ${JSON.stringify(entries)}. 
+        Answer the user question: ${question} 
+        (Respond with HTML formatting, do not include any tildes or markdown formatting.)`;
+        
+        const response = await axios.post(
+            'https://openrouter.ai/api/v1/chat/completions',
+            {
+                model: 'google/gemma-3n-e2b-it:free',
+                messages: [
+                    { role: 'user', content: prompt }
+                ]
+            },
+            {
+                headers: {
+                    'Authorization': `Bearer ${process.env.OPENROUTER_API_KEY}`,
+                    'Content-Type': 'application/json'
+                }
+            }
+        );
+        let cleanText = response.data.choices?.[0]?.message?.content || 'No answer.';
+        if (typeof cleanText === 'string') {
+            cleanText = cleanText.replace(/```html\s*/gi, '').replace(/```/g, '');
+        }
+
+        res.json({ answer: cleanText });
+    } catch (error) {
+        console.error('Batch chatbot error:', error?.response?.data || error.message);
         res.status(500).json({ error: 'Failed to get response from LLM.' });
     }
 });
